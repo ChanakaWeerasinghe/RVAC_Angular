6ed34d2e2cf1b5512b0e6e48ea40a4758729abb4
c_weerasinghe <chanaka.weerasinghe@hitachi-dps.com>
2023-11-06 11:10:48 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
-
+import { Component,VERSION } from '@angular/core';
+// @ts-ignore
+import {callJSFun} from '../custom.js';
+// import {productData} from '../custom.js';
+ declare function myfunction(param1: string, param2: string): any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+version: object = VERSION;
+
+
+  srcsetConfig = [
+    { size: '112/112', query: '600w' },
+    { size: '500/500', query: '' }
+  ];
+
+  products: any[] = [
+    {
+      name: 'Sterling Silver Necklace',
+      description: 'Gifts for Girls Sterling Silver Necklace',
+      price: 144.99,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDngLv-AwYkNHYzNdTg0az3VKs1g1tPHzELw&usqp=CAU'
+    },
+    {
+      name: 'Ring-Minimalistic',
+      description: ' Handmade Flower Signet Ring-Minimalistic',
+      price: 144.99,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSg56EzsnBsMA03uOn6wufLLGgvH6FDWFwfowbYt3n5sEZE7Pe-OYF7a490lHt5DL62Q4I&usqp=CAU'
+    },
+    {
+      name: 'Jewelry',
+      description: 'Promise Lover Jewelryd',
+      price: 11.99,
+      image: 'https://jewellerymatters.com/cdn/shop/products/RSSCZ802-3_300x300.jpg?v=1638930920'
+    },
+    {
+      name: 'Jewelry',
+      description: 'A delicious food',
+      price: 244.99,
+      image: 'https://s.alicdn.com/@sc04/kf/H60cc5836af0546a78c51246ae1c02b4be.jpg_200x200.jpg'
+    },
+    {
+      name: 'Jewelry',
+      description: 'A delicious food',
+      price: 74.99,
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKYyOxHhGhl7GcvAm9RNOnDgMyTxHOnSNW7w&usqp=CAU'
+    },
+  ]
+
+
   title = 'angular-tour-of-heroes';
+    ngOnInit(){
+
+
+    callJSFun();
+     }
+
+     callJSFunction(){
+     console.log("_________");
+         callJSFun();
+          }
+
+
+           Status: boolean = true;
+
+ clicked(data: string) {
+ console.log(data);
+               myfunction(data, 'test2');
+//              productData("data data");
+                this.Status = false;
+             }
+
 }
+
+
--- src/custom.js
+++ src/custom.js
@@ -1,15 +1,14 @@
 
 exports.callJSFun=function(){
-window.addEventListener('flutterInAppWebViewPlatformReady', function (event) {
-    window.flutter_inappwebview
-      .callHandler('handlerFoo')
+     window.flutter_inappwebview
+      .callHandler('send_token')
       .then(function (result) {
         // print to the console the data coming
         // from the Flutter side.
         console.log(JSON.stringify(result));
 
         window.flutter_inappwebview.callHandler(
-          'handlerFooWithArgs',
+          'back_to_me_angular',
           1,
           true,
           ['bar', 5],
@@ -17,40 +16,47 @@ window.addEventListener('flutterInAppWebViewPlatformReady', function (event) {
           result
         );
       });
-  });
-hello();
-    console.log("Hello Call JS Function From TypeScript ");
+
 }
 
-function hello() {
- print("sdfsdfds");
-  function sendMessageToFlutter() {
-    var message = 'Hello, Flutter! I am a message from the WebView!';
-    window.flutter_inappwebview.callHandler('messageHandler', message);
-  }
 
-  window.addEventListener('flutterInAppWebViewPlatformReady', function (event) {
-    window.flutter_inappwebview
-      .callHandler('handlerFoo')
-      .then(function (result) {
-        // print to the console the data coming
-        // from the Flutter side.
-        console.log(JSON.stringify(result));
+  exports.productData=function(){
+              window.flutter_inappwebview.callHandler(
+                'product_data',
+                data,
+              {
+                "products": {
+                  "data": {
+                    "items": [{
+                      "id": "GGOEAFKA087499",
+                      "name": "Android Small Removable Sticker Sheet",
+                      "description": "Show your Android pride by placing these 8 fun stickers on your technology products or accessories!",
+                      "features": "<p>8 Android stickers</p>\n<p>White colored sticker sheet</p>",
+                      "price": "2.99",
+                      "keywords": "Android Small Removable Sticker Sheet, android stickers, sticker sheets, removable sticker sheets, small sticker sheet, android small sticker sheets, Android Sheet",
+                      "url": "Android+Small+Removable+Sticker+Sheet",
+                      "category": "accessories",
+                      "subcategory": "accessories"
+                    },]}}}
+              );
+              window.flutter_inappwebview.callHandler(
+                'client_id',
+                "hgsdfh787834ndns",
 
-        window.flutter_inappwebview.callHandler(
-          'handlerFooWithArgs',
-          1,
-          true,
-          ['bar', 5],
-          { foo: 'baz' },
-          result
-        );
-      });
-  });
-
-  //   alert('Hello!!!');
+              );
 }
 
-hello();
+
+function myfunction(params1, params2) {
+    console.log('param1', params1);
+    console.log('param2', params2);
+      window.flutter_inappwebview.callHandler(
+                    'product_data',
+                    params1,
+                    params2,
+
+                  );
+}
+
 
 
